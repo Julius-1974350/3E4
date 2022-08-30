475dbce19d54c9264e1538d721873d9264f4c12f
Julius-1974350 <1974350@cstj.qc.ca>
2022-08-30 09:45:47 -0400

    fin du cours 30 aout

---
+++ server.js
@@ -0,0 +1,47 @@
+import chalk from "chalk";
+
+import app from './src/app.js';
+
+const PORT = 4200;
+
+app.listen(PORT, err => {
+	if(err){
+		//nous avons une erreur, on pourrait faire du code plus brillant.
+		process.exit(1);
+	}
+	console.log(chalk.rgb(255, 10, 120)('Server listening on port ${PORT}'));
+});
+
+console.log("Test");
+
+const log = console.log;
+
+// Combine styled and normal strings
+log(chalk.blue('Hello') + ' World' + chalk.red('!'));
+
+// Compose multiple styles using the chainable API
+log(chalk.blue.bgRed.bold('Hello world!'));
+
+// Pass in multiple arguments
+log(chalk.blue('Hello', 'World!', 'Foo', 'bar', 'biz', 'baz'));
+
+// Nest styles
+log(chalk.red('Hello', chalk.underline.bgBlue('world') + '!'));
+
+// Nest styles of the same type even (color, underline, background)
+log(chalk.green(
+	'I am a green line ' +
+	chalk.blue.underline.bold('with a blue substring') +
+	' that becomes green again!'
+));
+
+// ES2015 template literal
+log(`
+CPU: ${chalk.red('90%')}
+RAM: ${chalk.green('40%')}
+DISK: ${chalk.yellow('70%')}
+`);
+
+// Use RGB colors in terminal emulators that support it.
+log(chalk.rgb(123, 45, 67).underline('Underlined reddish color'));
+log(chalk.hex('#DEADED').bold('Bold gray!'));
